22a56fa9d3bd311999aa5d77e5f80fc96daaf08c
daajem <jimenezdaniel772@gmail.com>
2024-03-15 17:02:50 -0700

    Agego practica 20

---
+++ practica20/actividad20.js
@@ -0,0 +1,14 @@
+var num1 = 5;
+var num2 = 8;
+
+if (num1 < num2) {
+    alert(num1 + " no es mayor que " + num2);
+}
+
+if (num2 <= 0) {
+    alert(num2 + " es positivo");
+}
+
+if (++num1 < num2) {
+    alert("Incrementar un numero mas num1, no lo hace mayor que num2" )
+}

3b5b19f647b1ea48b254f108a4b6bee94ff369ae
daajem <jimenezdaniel772@gmail.com>
2024-03-15 17:40:03 -0700

    Agrego practica 17

---
+++ practica17/actividad17.js
@@ -0,0 +1,25 @@
+// Daniel Jimenez Manrique
+function operacionesMatematicas () {
+    var num1 = 10;
+    var num2 = 10;
+
+    suma = num1 + num2;
+    resta = num1 - num2;
+    multiplicacion = num1 * num2;
+    division = num1 / num2;
+
+    alert("Suma: " + suma + "\nResta: " + resta + "\nMultplicacion:" + multiplicacion + "\nDivision: " + division);
+}
+
+function operacionesMatematicas2 () {
+    var num1 = parseInt(prompt("Ingrese el primer numero"));
+    var num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+    suma = num1 + num2;
+    resta = num1 - num2;
+    multiplicacion = num1 * num2;
+    division = num1 / num2;
+
+    alert("Suma: " + suma + "\nResta: " + resta + "\nMultplicacion:" + multiplicacion + "\nDivision: " + division);
+
+}

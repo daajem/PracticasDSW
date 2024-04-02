da79d5b791f46fe2da339a22d80185a3caecf187
daajem <jimenezdaniel772@gmail.com>
2024-04-01 19:06:19 -0700

    Se agrega la practica 24

---
+++ practica24/actividad24.js
@@ -0,0 +1,12 @@
+var hayUnHuracan = true
+function Aviso_De_Huracam(hayUnHuracan) {
+    
+
+    if (hayUnHuracan) {
+        alert("Hay un Huracan");
+    }else{
+        alert("Esta Soleado");
+
+    }
+    
+}

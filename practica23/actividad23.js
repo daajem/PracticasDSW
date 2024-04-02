bb220fe01e20ecf29c00e4513e609d4532a618a3
daajem <jimenezdaniel772@gmail.com>
2024-04-01 19:05:56 -0700

    Se agrega la practica 23

---
+++ practica23/actividad23.js
@@ -0,0 +1,11 @@
+function numeroParImpar() {
+    var numero = 0;
+    numero = parseInt(prompt("Ingrese el numero"));
+    
+    if((numero % 2) == 0){
+        alert(numero + " es par");
+    }else{
+      alert(numero + " es impar");
+    } 
+}    
+
